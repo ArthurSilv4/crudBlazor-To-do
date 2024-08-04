fcd13b73d495a2ffaf5ef76cdb0f6012e97452aa
Arthur de Souza Silva <arthurdesouza.silv@gmail.com>
2024-08-04 18:46:21 -0300

    Adicionar arquivos de projeto.

---
+++ crudBlazor/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using crudBlazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crudBlazor.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; } = null!;
+    }
+}
